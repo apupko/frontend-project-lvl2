5ffa488df157c1c57dbf63c1d8c70fd792e501e6
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-15 09:38:43 +0600

    Add -f option and filepaths args

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,8 @@ import program from 'commander';
 program.description('Compares two configuration files and shows a difference');
 program.version('1.0.0');
 program.helpOption('-h, --help', 'output usage information');
+program.arguments('<filepath1> <filepath2');
+program.option('-f, --format [type]', 'output format');
 const run = () => {
   program.parse(program.args);
 };
