2cac7d5a4991a0898c992b7ca7123f374672e656
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-17 15:27:11 +0600

    Change pasing flow

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import fs from 'fs';
 import path from 'path';
+import parse from './parsers.js';
 import getDiffString from './gendiff.js';
 
 const genDiff = (filepath1, filepath2) => {
   const firstFile = fs.readFileSync(path.resolve(filepath1), 'utf8');
   const secondFile = fs.readFileSync(path.resolve(filepath2), 'utf8');
-
-  const firstObj = JSON.parse(firstFile);
-  const secondObj = JSON.parse(secondFile);
-
-  return getDiffString(firstObj, secondObj);
+  let result = '';
+  try {
+    const firstObj = parse(firstFile);
+    const secondObj = parse(secondFile);
+    result = getDiffString(firstObj, secondObj);
+  } catch (e) {
+    console.log(e.message);
+  }
+  return result;
 };
 
 export default genDiff;
