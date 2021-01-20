6c6542e093978adc2dee2402752e02cd73ce4a3e
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-20 20:14:22 +0600

    Add stylish format as default in genDiff()

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
 import getDifference from './gendiff.js';
-import format from './formaters.js';
+import format from './formaters/formaters.js';
 
-const genDiff = (filepath1, filepath2, formatType) => {
+const genDiff = (filepath1, filepath2, formatType = 'stylish') => {
   const firstFile = fs.readFileSync(path.resolve(filepath1), 'utf8');
   const secondFile = fs.readFileSync(path.resolve(filepath2), 'utf8');
   let result = '';
