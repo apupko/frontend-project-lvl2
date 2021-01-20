15bee99205597de2ec12aa80509500ee0431049a
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-21 00:59:37 +0600

    Add json formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,11 @@
 import formatToStylish from './stylish.js';
 import formatToPlain from './plain.js';
-
+import formatToJson from './json.js';
 const format = (nodes, style) => {
   const mappingFormat = {
     stylish: formatToStylish(nodes),
     plain: formatToPlain(nodes),
+    json: formatToJson(nodes),
   };
   return mappingFormat[style];
 };
