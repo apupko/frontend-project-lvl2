00108df0d949a8296b8ec54e03452be9dd80d4a6
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-20 13:48:21 +0600

    Create test for no-flat diff

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,29 +1,20 @@
 import { readFixtureFile } from '../__fixtures__/utils.js';
-import { getDifferences, genString } from '../src/gendiff.js';
+import getDifference from '../src/gendiff.js';
 
 let firstJson;
 let secondJson;
-let expectedObj;
-let expectedText;
+let expectedJson;
 
 beforeAll(() => {
   firstJson = readFixtureFile('file1.json');
   secondJson = readFixtureFile('file2.json');
-  expectedText = readFixtureFile('expected.txt');
-
-  expectedObj = {
-    follow: 'removed',
-    host: 'no_change',
-    proxy: 'removed',
-    timeout: 'modify',
-    verbose: 'added',
-  };
+  expectedJson = readFixtureFile('expected_difference.json');
 });
-test('Generate diff from 2 obj', () => {
+
+test('Generate difference from 2 obj', () => {
   const firstObj = JSON.parse(firstJson);
   const secondObj = JSON.parse(secondJson);
-  const diffObj = getDifferences(firstObj, secondObj);
+  const expectedObj = JSON.parse(expectedJson);
+  const diffObj = getDifference(firstObj, secondObj);
   expect(diffObj).toEqual(expectedObj);
-  const diffString = genString(firstObj, secondObj, diffObj);
-  expect(diffString).toEqual(expectedText);
 });
